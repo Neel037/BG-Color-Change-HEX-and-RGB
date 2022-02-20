e40b2d8be83afe48276176d9f3cc6c6149c25741
Mijanur Rahman <mijanurrahman05@gmail.com>
2022-02-21 00:44:47 +0600

    BG Color Change

---
+++ README.md
@@ -0,0 +1,6 @@
+<h1> Changing Background Color<h1/>
+  
+Project Requirements: Change the background colorby generating random RGB or HEX color by clicking a Button and Display the color codes in RGB and HEX
+
+  
+ 
---
+++ index.js
@@ -0,0 +1,48 @@
+/**
+ * Project Requirements:
+ * --- Change the background colorby generating random Hex and RGB color by clicking a Button and display the hex code in a field
+ */
+
+// Step 1: Create onLoad handler
+// Step 2: Random Color Generator Function
+// Step 3: Collect all necessary references
+// Step 4: Handle the click event
+
+window.onload = () =>{
+    main();
+}
+
+
+
+function main(){
+
+    const root = document.getElementById('root');
+    const btn = document.getElementById('cng-btn');
+    const output = document.getElementById('output');
+    const output2 = document.getElementById('output2');
+
+    btn.addEventListener('click', function(){
+        const bgColor = generateHEX();
+        rgbColor = bgColor[0];
+        hexColor = bgColor[1];
+
+
+        root.style.backgroundColor = rgbColor;
+        output.value = hexColor;
+        output2.value = rgbColor;
+    });
+
+}
+
+
+
+
+function generateHEX(){
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+
+
+
+    return [`rgb(${red}, ${green}, ${blue})`, `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`];
+}
